0cd0c12c9cf4de9709b32c715a1ecd420d89f363
Saurya Acharya <sacharya@noggin.com.au>
2021-02-19 09:05:51 +1100

    used only library built

--- projects/ngx-uploader/src/public_api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of ngx-uploader
- */
-
-export * from './lib/interfaces';
-export * from './lib/ng-file-drop.directive';
-export * from './lib/ng-file-select.directive';
-export * from './lib/ngx-uploader.class';
-export * from './lib/ngx-uploader.module';
